271fa88b5c9bc5f2ff7f314afd2d5cbe2fa5221e
dinhhailinh <58936308+dinhhailinh@users.noreply.github.com>
2022-01-26 17:55:47 +0700

    add validate

--- backend/controller/upload.js
+++ backend/controller/upload.js
@@ -2,7 +2,7 @@
 const upload = async (req, res) => {
     try {
         await res.status(200).json(`upload success: ${req.file.path}`)
-          
+
     } catch (error) {
         res.status(500).json(error)
     }
--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const { register, login, logout } = require('../controller/auth')
+const { validateRegisterRequest, validateLoginRequest } = require('../middleware/validator')
 
 // REGISTER ROUTE
-router.post('/auth/register', register) 
+router.post('/auth/register', validateRegisterRequest,  register) 
 
 // LOGIN ROUTE
-router.post('/auth/login', login)
+router.post('/auth/login', validateLoginRequest, login)
 
 //LOGOUT ROUTE
 router.post('/auth/logout', logout)
