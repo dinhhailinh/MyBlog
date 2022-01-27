521d651424f4d0cb7de27d5f464bda0f4721f4f7
dinhhailinh <58936308+dinhhailinh@users.noreply.github.com>
2022-01-27 15:42:48 +0700

    fix validate

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const { register, login, logout } = require('../controller/auth')
-const { validateRegisterRequest, validateLoginRequest } = require('../middleware/validator')
+const { validateRegisterRequest, validateLoginRequest, isRequestValidated } = require('../middleware/validator')
 
 // REGISTER ROUTE
-router.post('/auth/register', validateRegisterRequest,  register) 
+router.post('/auth/register', validateRegisterRequest, isRequestValidated,register) 
 
 // LOGIN ROUTE
-router.post('/auth/login', validateLoginRequest, login)
+router.post('/auth/login', validateLoginRequest, isRequestValidated,login)
 
 //LOGOUT ROUTE
 router.post('/auth/logout', logout)
--- backend/router/categories.js
+++ backend/router/categories.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const { createCategory } = require('../controller/categories')
+const { requireLogin, adminMiddleware } = require('../middleware/checkAuth')
 
 // CREATE CATEGORY ROUTE
-router.post('/createCategory', createCategory) 
+router.post('/createCategory', requireLogin, adminMiddleware,createCategory) 
 
 module.exports = router
